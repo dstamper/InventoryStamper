fc2d74b79a9b386908769e80ea5dcc2b1caa3919
David Stamper <stamper.dt@gmail.com>
2019-03-20 09:11:19 -0700

    Added SQLite nuget package

--- src/InventoryStamper.App/App.xaml.cs
+++ src/InventoryStamper.App/App.xaml.cs
@@ -13,5 +13,10 @@ namespace InventoryStamper.App
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Console.WriteLine("I have the power");
+        }
     }
 }
